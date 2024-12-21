a42688b455dcc569af1f3fbda5fdae9a25108dda
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-12-21 13:33:55 -0300

    Rota listar todos

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,11 +1,13 @@
+const database = require('../models');
 class PessoaController {
-    static async pegarTodos(req, res) {
-        try {
-            
-        } catch (error) {
-            
-        }
-    }
+  static async pegarTodos(req, res) {
+    try {
+      const pessoas = await database.Pessoa.findAll();
+      console.log('Aqui');
+
+      return res.status(200).json(pessoas);
+    } catch (error) {}
+  }
 }
 
 module.exports = PessoaController;
